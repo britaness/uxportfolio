8873138651d21a1363704d008549c4dd10109832
Brita Ness <britarness@gmail.com>
2018-06-26 16:41:52 -0700

    lots o changes

--- myScript.js
+++ myScript.js
@@ -14,31 +14,31 @@ window.addEventListener("scroll", myScrollFunc);
 
 
 
-//FADE IN
-$(function(){  // $(document).ready shorthand
-  $('.work').fadeIn('slow');
-});
-
-$(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.projectTile').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it */
-            if( bottom_of_window >= 1400 ){
-
-                $(this).animate({'opacity':'1'},1000);
-
-            }
-
-        });
-
-    });
-
-});
+// //FADE IN
+// $(function(){  // $(document).ready shorthand
+//   $('.work').fadeIn('slow');
+// });
+//
+// $(document).ready(function() {
+//
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+//
+//         /* Check the location of each desired element */
+//         $('.projectTile').each( function(i){
+//
+//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//             /* If the object is completely visible in the window, fade it */
+//             if( bottom_of_window >= 1400 ){
+//
+//                 $(this).animate({'opacity':'1'},1000);
+//
+//             }
+//
+//         });
+//
+//     });
+//
+// });
